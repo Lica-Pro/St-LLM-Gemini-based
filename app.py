dffb083eca1e0a1a1118c072012cbeaedf999c11
Lica-Pro <assershahin674@gmail.com>
2026-06-22 16:13:43 +0100

    Set up session state for messages in app.py
    Initialize session state for user and AI messages.

--- app.py
+++ app.py
@@ -1,4 +1,11 @@
 import streamlit as st
 import google.generativeai as genai
-
 st.set_page_config(page_title="Lica-Pro AI assistant genai-based")
+state = st.session_state
+if "user_messages" not in state:
+  state.memory = []
+  state.user_messages = []
+  state.ai_messages = []
+with st.sidebar:
+  st.title("Type your API key to start")
+  st.write("You can get those in https://aistudio.google.com/app/api-keys")
